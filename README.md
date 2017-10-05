19af6351349b3452f2b99d4589e8c5b66c6894e8
Sean La <seanla1996@gmail.com>
2017-10-04 20:23:01 -0700

    solver now has test function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # principal-agent
 Repeated Moral Hazard Relationship in a Large-Scale Shortest Path Problem
+
+## Dependencies
+- [CVXPY 1.0](http://www.cvxpy.org/en/latest/)
--- solver.py
+++ solver.py
@@ -1,12 +1,26 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 # Author: Sean La
 
 import getopt
 import sys
 import cvxpy as cvx
 import numpy as np
+import itertools
 
-def get_possible_policies(num_states, num_actions):
+# Global variables for problem_specs dict keys
+NUM_STATES = 'num_states'
+NUM_ACTIONS = 'num_actions'
+TRANS_MATRIX = 'trans_matrix'
+INITIAL_PROB = 'initial_prob'
+REWARDS = 'rewards'
+COSTS = 'costs'
+PART_CONST = 'part_const'
+MAX_CONTRACT = 'max_contract'
+ACTION_MAP = 'action_map'
+
+def get_possible_policies(problem_specs):
+    num_states = problem_specs[NUM_STATES]
+    num_actions = problem_specs[NUM_ACTIONS]
     action_list = ['action_%d' % (action) for action in range(num_actions)]
     # Cartesian product of the action_list with itself num_states times is equivalent to finding all possible policies
     policies = [policy for policy in itertools.product(action_list, repeat = num_states)]
@@ -20,35 +34,74 @@ def get_optimal_contracts_for_policies(policies, problem_specs):
     return contracts
 
 def determine_optimal_contract(policy, problem_specs):
-    num_states = problem_specs['num_states']
+    num_states = problem_specs[NUM_STATES]
     # trans_matrix is a num_states x num_states x num_actions size array
-    prob = problem_specs['trans_matrix']
-    initial_prob = problem_specs['initial_prob']
+    prob = problem_specs[TRANS_MATRIX]
+    initial_prob = problem_specs[INITIAL_PROB]
     # rewards is a num_states x num_states size matrix
-    rewards = problem_specs['rewards']
+    rewards = problem_specs[REWARDS]
     # trans_matrix is a num_states x num_states x num_actions size array
-    costs = problem_specs['costs']
+    costs = problem_specs[COSTS]
     # agent participation constraint
-    part_const = problem_specs['part_const']
+    part_const = problem_specs[PART_CONST]
     # maximum contracts vector
-    max_contract = problem_specs['max_contract']
+    max_contract = problem_specs[MAX_CONTRACT]
+    # Get the policy map
+    action_map = problem_specs[ACTION_MAP]
     # Create the contract vector
-    contracts = Variable(num_states)
-
+    contracts = cvx.Variable(num_states)
     # Create the objective function
-    principal_util = [ prob[:,policy[i],i]*(reward[i,:]-contracts) for i in range(num_states) ]
+    principal_util = [ prob[ :, i, action_map[policy[i]] ]*(rewards[i,:]-contracts) for i in range(num_states) ]
     objective = cvx.Maximize(initial_prob*principal_util) 
     # Construct the constraints
-    agent_util = [ prob[:,policy[i],i]*(contracts-costs[i,:]) for i in range(num_states) ]
+    agent_util = [ prob[ :, i, action_map[policy[i]] ]*(contracts-costs[i,:]) for i in range(num_states) ]
     constraints = [ initial_prob*agent_util > part_const, contracts < max_contract ]
     solution = cvx.Problem(objectice,constraints)
-
     return solution
-        
+
+def get_test_problem_specs():
+    num_actions = 2
+    actions = [ "action_%d" % (action) for action in range(num_actions) ]
+    num_states = 2
+    problem_specs = {NUM_ACTIONS: num_actions, NUM_STATES: num_states} 
+    policies = get_possible_policies(problem_specs)
+    action_map = {action: actions.index(action) for action in actions}
+    trans_matrix = np.zeros( (num_states,num_states,len(policies)) )
+    for i in range(len(policies)):
+        trans_matrix[:,:,i] = [[0.5,0.5],[0.3,0.7]]
+    rewards = np.array([[5,5],[1,1]])
+    costs = np.array([ [2,2],[3,3] ])
+    part_const = 2
+    max_contract = np.array([3,3])
+    initial_prob = np.array([0.5,0.5])
+
+    problem_specs = {
+        NUM_ACTIONS: num_actions,
+        NUM_STATES: num_states,
+        ACTION_MAP: action_map, 
+        TRANS_MATRIX: trans_matrix,
+        REWARDS: rewards,
+        COSTS: costs,
+        PART_CONST: part_const,
+        MAX_CONTRACT: max_contract,
+        INITIAL_PROB: initial_prob,
+        }
+
+    return problem_specs
+
+def read_config_file(config_path):
+    return None
+
+def print_solution(solution):
+    return None
+
+def write_solution(solution,output_path):
+    return None
+
 def main(argv):
     help_message = "Description: Repeated Moral Hazard Relationship in Large-Scale Shortest Path Problem"
-    usage_message = "Usage: %s [-h help and usage]" % (sys.argv[0])
-    options = "hi:o:"
+    usage_message = "Usage: %s [-h help and usage] [-t test] [-i configuration file] [-o output path]" % (sys.argv[0])
+    options = "hti:o:"
 
     try:
         opts, args = getopt.getopt(sys.argv[1:],options)
@@ -61,13 +114,45 @@ def main(argv):
         print(usage_message)
         sys.exit(1)
 
-    problem_specs = {'trans': trans_matrix, 'rewards': rewards, 'costs': costs, 'agent_part': agent_part, \
-                     'max_contract': max_contract, 'num_states': num_states, 'num_actions': num_actions, \
-                     'lambda': lmbda }
+    config_path = None
+    output_path = None
+    do_test = False
 
-    possible_policies = get_possible_policies(num_states, num_actions)
-    optimal_contracts = get_optimal_contracts_for_policies(possible_policies, problem_specs)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(help_message)
+            print(usage_message)
+            sys.exit(0)
+        elif opt == '-t':
+            do_test = True
+        elif opt == '-i':
+            config_path = arg
+        elif opt == '-o':
+            output_path = arg 
+
+    opts_incomplete = False
+
+    if not do_test:
+        if config_path is None:
+            print("Error: please provide a configuration file.")
+            opts_incomplete = True
+        if output_path is None:
+            print("Error: please specify a path for the output file.")
+            opts_incomplete = True 
+    if opts_incomplete:
+        print(usage_message)
+        sys.exit(1)
+
+    if (do_test):
+        problem_specs = get_test_problem_specs()
+    else:
+        problem_specs = read_config_file(config_path)
+    possible_policies = get_possible_policies(problem_specs)
+    optimal_contracts = get_optimal_contracts_for_policies(possible_policies,problem_specs)
     solution = determine_optimal_policy_and_contract(optimal_contracts)
+    print_solution(solution)
+    if (not do_test):
+        write_solution(solution,output_path)
 
 if __name__ == "__main__":
     main(sys.argv)
